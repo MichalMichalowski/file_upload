24a73a9d2fe8dbf6d7cccec15c0a53e88e290843
MichalMichalowski <michalowskimichal688@gmail.com>
2024-01-06 21:22:25 +0100

    cors configuration

---
+++ spring_backend/src/main/java/fileuploader/app/demo/config/AppConfig.java
@@ -0,0 +1,21 @@
+package fileuploader.app.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
